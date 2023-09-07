90d4d5b27a7a3ed2e68448acf595eed41fe73b31
xlvxiii <pangestualviansyah@gmail.com>
2023-09-07 19:55:29 +0700

    done

--- main.dart
+++ main.dart
@@ -6,10 +6,12 @@ class Mahasiswa {
   Mahasiswa(this.nim, this.nama, this.tahunLahir);
 
   void perkenalan() {
-    print("Perkenalkan, nama saya $nama");
+    int usia = DateTime.now().year - tahunLahir;
+    print("Perkenalkan, nama saya $nama dengan NIM $nim dan usia saya $usia tahun.");
   }
 }
 
 void main() {
-  var mhs = Mahasiswa("H1D020068", "Abdalhaqq Muhammad Saih", 2002);
+  Mahasiswa mhs = Mahasiswa("H1D021024", "Alviansyah Pangestu", 2003);
+  mhs.perkenalan();
 }
